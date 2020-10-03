e6cbad4d5219938fdbf6fc292703e7ab3b1cdcd8
ra-kennethlopez <ra.kennethlopez@gmail.com>
2020-10-03 21:21:14 +0800

    Add migration hr_sss_sub_id to avoid duplicate id on add gsis loan types

---
+++ root/database/migrations/2020_10_03_204502_update_sequnece_hr_sss_sub_id.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSequneceHrSssSubId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $seqStart = $this->getLastId() + 1;
+        
+        /**
+        * auto increments id and avoid possilbe duplicate value using $seqStart
+        * 
+        * note: we can't drop the sequence hris.hr_sss_sub_id_seq cause id might be dependent on it 
+        * and we can't do a drop cascade cause we will lose data, that's why we do a create if not exists and alter sequnce as a work around
+        */
+        DB::statement("create sequence if not exists hris.hr_sss_sub_id_seq increment 1 minvalue 1 maxvalue 2147483647 start " . $seqStart);
+        DB::statement("alter sequence hris.hr_sss_sub_id_seq restart with " .$seqStart);
+        DB::statement("alter table hris.hr_sss_sub alter id set default nextval('hris.hr_sss_sub_id_seq')");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // do nothing
+    }
+
+    // returns maximum value of id
+    public function getLastId() {
+        return DB::table('hris.hr_sss_sub')
+            ->select('id')
+            ->orderBy('id', 'desc')
+            ->first()
+            ->id;
+    }
+}
